52badff5f51f0cc72a2d9e3cc69a9d678ec7daa2
fathsam82 <fathsam82@gmail.com>
2023-05-21 22:05:07 -0600

    MakeChange Complete

---
+++ READ.md
@@ -0,0 +1,15 @@
+# Make changeProject
+The overall idea for this project was to make a simple cash register application that was based off of the item Price and the amount tendered for that item.
+
+## Overview
+description of project
+There were three main routes of this project. The first condition considered if the customer provided exact change for an item. It simply output a thankyou statement to the customer and to have a nice day. The second condition, if the customer paid with an amount that required change to be given back, was where I called on a separate method called changeMaker. This is were the bulk of the coding took place. I stored the amount of each denominations needed to give back to the customer in a variable called twentyCounter, tenCount, etc. With each if block within the called method, I updated remainder and declared that amount as tendered, and repeated the process. In the third main condition, if the required item price was not met with the money tendered from the customer, they get an error message.
+
+## Technologies Used
+-Java
+-Eclipse
+
+## Lessons Learned
+I reinforced my understanding of calling on methods and casting variables.
+
+
--- src/MakeChange.java
+++ src/MakeChange.java
@@ -1,9 +1,125 @@
+import java.util.Scanner;
 
 public class MakeChange {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("What is the price of this item?");
+		double itemPrice = sc.nextDouble();
+		System.out.println("How much money have you provided for the item");
+		double moneyTendered = sc.nextDouble();
+		double diff = moneyTendered - itemPrice;
+		sc.close();
 
+		if (moneyTendered == itemPrice) {
+			System.out.println("Thank you for your business. Have a nice day");
+
+		} else if (moneyTendered > itemPrice) {
+			System.out.println("Result: " + changeMaker(diff));
+
+		} else if (moneyTendered < itemPrice) {
+			System.out.println("Result: Error message");
+		}
 	}
 
+	public static String changeMaker(double tendered) {
+		String result = "";
+		double remainder = tendered;
+		int twentyCount = (int) (tendered / 20.00);
+		if (twentyCount > 0) {
+			if (twentyCount > 1) {
+				result += twentyCount + " twenty dollar bills, ";
+			} else {
+				result += twentyCount + " twenty dollar bill, ";
+			}
+			
+			remainder = tendered % 20.00;
+		}
+		tendered = (remainder);
+		int tenCount = (int) (tendered / 10.00);
+		if (tenCount > 0) {
+			if (tenCount > 1) {
+				result += tenCount + " ten dollar bills, ";
+			} else {
+				result += tenCount + " ten dollar bill, ";
+			}
+
+			remainder = tendered % 10.00;
+		}
+		tendered = (remainder);
+
+		int fiveCount = (int) (tendered / 5.00);
+		if (fiveCount > 0) {
+			if (fiveCount > 1) {
+				result += fiveCount + " five dollar bills, ";
+			} else {
+				result += fiveCount + " five dollar bill, ";
+			}
+
+			remainder = tendered % 5.00;
+		}
+		tendered = (remainder);
+
+		int oneCount = (int) (tendered / 1.00);
+		if (oneCount > 0) {
+			if (oneCount > 1) {
+				result += oneCount + " one dollar bills, ";
+			} else {
+				result += oneCount + " one dollar bill, ";
+			}
+
+			remainder = tendered % 1.00;
+		}
+		tendered = remainder + .00005;
+
+		int quarterCount = (int) (tendered / 0.25);
+		if (quarterCount > 0) {
+			if (quarterCount > 1) {
+				result += quarterCount + " quarters, ";
+			} else {
+				result += quarterCount + " quarter, ";
+			}
+
+			remainder = tendered % 0.25;
+		}
+		tendered = remainder;
+
+		int dimeCount = (int) (tendered / 0.10);
+		if (dimeCount > 0) {
+			if (dimeCount > 1) {
+				result += dimeCount + " dimes, ";
+			} else {
+				result += dimeCount + " dime, ";
+			}
+
+			remainder = tendered % 0.10;
+		}
+		tendered = remainder;
+
+		int nickelCount = (int) (tendered / 0.05);
+		if (nickelCount > 0) {
+			if (nickelCount > 1) {
+				result += nickelCount + " nickels, ";
+			} else {
+				result += nickelCount + " nickel, ";
+			}
+
+			remainder = tendered % 0.05;
+		}
+		tendered = remainder + .000051;
+
+		int pennyCount = (int) (tendered / 0.01);
+		if (pennyCount > 0) {
+			if (pennyCount > 1) {
+				result += pennyCount + " pennies. ";
+			} else {
+				result += pennyCount + " penny. ";
+			}
+			
+			remainder = tendered % 0.01;
+		}
+		tendered = remainder;
+		return result;
+	}
 }
+
